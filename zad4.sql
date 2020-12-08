f8ea1c0f31dc1160b4a9a8e7315b985c14d1f372
Your Name <toni@orqafpv.com>
2020-12-08 09:23:20 +0100

    Bug fix zad4

--- zad4.sql
+++ zad4.sql
@@ -2,6 +2,6 @@ SELECT	ime,
 		prezime,
 		naziv 
 FROM ispit, student, predmet
-WHERE ispit.ocjena >= 4 AND ispit.mbrStud = student.mbr;
+WHERE ispit.ocjena >= 4 AND ispit.mbrStud = student.mbr AND ispit.sifPredmeta=predmet.sifra;
 
 	
